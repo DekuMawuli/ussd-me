75e74abae0568094ce4e053abe363bd39c7b3183
Deku Mawuli <dekumedikal@gmail.com>
2022-07-16 01:00:16 +0000

    Added first menu

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo "App is running";
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
+
+$response = "";
+
+if ($text == ""){
+    $resp = "Welcome to Seritrex Applio System.\n";
+    $res .= "Kindly Select your options: \n";
+    $res .= "1. Register With Us \n";
+    $res .= "2. Request Payment \n";
+    $res .= "3. Go for bailout \n";
+    echo $res;
+}
